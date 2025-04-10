3c97985e1926ae7abf00a657e7dd528e4fc50044
shaheerali838 <shaheer838838@gmail.com>
2025-04-10 23:42:54 +0500

    Dimagh kharab ho rha h....

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -3,11 +3,11 @@ import gsap from "gsap";
 import React, { useRef, useState } from "react";
 import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
+import { Canvas } from "@react-three/fiber";
 
 import * as THREE from "three";
 import { models, sizes } from "../constants";
 import { View } from "@react-three/drei";
-import { label } from "three/tsl";
 
 const Model = () => {
   useGSAP(() => {
@@ -36,7 +36,7 @@ const Model = () => {
   return (
     <section className="common-padding ">
       <div className="screen-max-width">
-        <h1 id="heading" className="section-heading ">
+        <h1 id="heading" className="section-heading mb-24">
           Take a Closer Look.
         </h1>
         <div className="flex flex-col items-center mt-5">
@@ -59,8 +59,8 @@ const Model = () => {
               item={model}
               size={size}
             />
-            <canvas
-              className="w-full h-full z-[-1]"
+            <Canvas
+              className="w-full h-full "
               style={{
                 position: "fixed",
                 top: 0,
@@ -69,13 +69,13 @@ const Model = () => {
                 right: 0,
                 overflow: "hidden",
               }}
-              eventsource={document.getElementById("root")}
+              eventSource={document.getElementById("root")}
             >
               <View />
-            </canvas>
+            </Canvas>
           </div>
           <div className="mx-auto w-full">
-            <p className="text-sm font-light text-center">{model.title}</p>
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
             <div className="flex-center">
               <ul className="color-container">
                 {models.map((item, i) => (
--- src/Components/ModelView.jsx
+++ src/Components/ModelView.jsx
@@ -1,6 +1,6 @@
 import { PerspectiveCamera, View, OrbitControls } from "@react-three/drei";
 import Lights from "./Lights";
-import Iphone from "./Iphone";
+import Iphone from "./Iphone.jsx";
 import { Suspense } from "react";
 import * as THREE from "three";
 
@@ -14,41 +14,39 @@ const ModelView = ({
   item,
 }) => {
   return (
-    <View
-      index={index}
-      id={gsapType}
-      className={`w-full  h-full ${index === 2 ? "right-[-100%]" : ""}`}
-    >
-      {/* Ambient Light */}
-      <ambientLight intensity={8} />
-      <directionalLight position={[10, 10, 10]} intensity={0.5} />
-
-      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
-      <Lights />
-      <OrbitControls
-        makeDefault
-        ref={controlRef}
-        enableZoom={false}
-        enablePan={false}
-        rotateSpeed={0.4}
-        target={new THREE.Vector3(0, 0, 0)}
-        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-      />
-
-      <group
-        ref={groupRef}
-        name={`${index === 1} ? 'small' : 'large' `}
-        position={[0, 0, 0]}
+    <>
+      <View
+        index={index}
+        id={gsapType}
+        className={`border-2 border-red-500 w-full h-full mb-3 ${
+          index === 2 ? "right-[-100%]" : ""
+        }`}
       >
-        <Suspense fallback={<div>Loading</div>}>
-          <Iphone
-            scale={index === 1 ? [5, 5, 5] : [5, 5, 5]}
-            position={[0, 0, 0]}
-            rotation={[0, Math.PI / 2, 0]}
-          />
-        </Suspense>
-      </group>
-    </View>
+        {/* Ambient Light */}
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+        <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+        <Lights />
+        <OrbitControls
+          makeDefault
+          ref={controlRef}
+          enableZoom={false}
+          enablePan={false}
+          rotateSpeed={0.4}
+          target={new THREE.Vector3(0, 0, 0)}
+          onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+        />
+        <group
+          ref={groupRef}
+          name={`${index === 1} ? 'small' : 'large' `}
+          position={[0, 0, 0]}
+        >
+          <Suspense fallback={<div>Loading</div>}>
+            <Iphone />
+          </Suspense>
+        </group>
+      </View>
+    </>
   );
 };
 
