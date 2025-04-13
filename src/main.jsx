4855d8f577c51db849a10ec1cdad9a47460a72bc
shaheerali838 <shaheer838838@gmail.com>
2025-04-13 16:33:45 +0500

    almost done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://0a3d9311a8f9e3aedc021d885bdce9a6@o4509145736740864.ingest.us.sentry.io/4509145737723904",
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -6,12 +6,16 @@ export const animateWithGsapTimeLine = (
   secondTarget,
   animationProps
 ) => {
+  console.time("GSAP timeline");
+
   timeline.to(rotationRef.current.rotation, {
     y: rotationState,
     duration: 1,
-    ease: 1,
+    ease: "power2.inOut",
   });
 
   timeline.to(firstTarget, { ...animationProps, ease: "power2.inOut" }, "<");
   timeline.to(secondTarget, { ...animationProps, ease: "power2.inOut" }, "<");
+
+  console.timeEnd("GSAP timeline");
 };
